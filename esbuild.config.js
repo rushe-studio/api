a0b548e26f69c3292e19663aeadbaadf0ca6ca6e
Arthur <chaff.arthur@gmail.com>
2024-07-30 00:19:39 +0200

    add esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,30 @@
+import process from 'process';
+import * as esbuild from 'esbuild';
+
+const isWatchMode = process.argv[2] === '--watch';
+
+async function run() {
+    const options = {
+        entryPoints: ['./src/index.ts'],
+        bundle: true,
+        outdir: './build',
+        tsconfig: './tsconfig.json',
+        format: 'esm',
+        loader: { '.ts': 'ts' },
+        allowOverwrite: true
+    }
+
+    if (isWatchMode) {
+        options.sourcemap = 'linked';
+        const ctx = await esbuild.context(options);
+        await ctx.watch();
+        console.log('Watching for changes...');
+    } else {
+        options.minify = true;
+        options.keepNames = true;
+        options.treeShaking = true;
+        await esbuild.build(options);
+    }
+}
+
+run();
